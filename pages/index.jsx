97c22933f7dbe7fde1e38ea01cad144f01bb3fa6
Lucas Vinicius <lucasvinicius211@yahoo.com>
2021-02-05 17:30:30 -0300

    Create root page

---
+++ pages/index.jsx
@@ -0,0 +1,13 @@
+function Home() {
+    return (
+        <div>
+            <PhraseDisplayer />
+        </div>
+    )
+}
+
+function PhraseDisplayer() {
+    return <p>Tem que se vacinar, sim!</p>
+}
+
+export default Home
